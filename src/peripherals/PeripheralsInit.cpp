bfa91cde73d5e838a44bd6a35ec9d0b9d198fdf3
atclarkson <atclarkson@gmail.com>
2026-06-18 10:25:19 +0700

    update button press logging

--- src/peripherals/PeripheralsInit.cpp
+++ src/peripherals/PeripheralsInit.cpp
@@ -4,6 +4,8 @@
 #include "lvgl/lvgl.h"
 #include "ui/screens/ui_ScreenMain.h"
 
+#define LOG_TAG "PeripheralsInit"
+#include "Logging.h"
 
 Mcp23017 g_mcp(MCP23017_ADDR);
 Horn     g_horn(&g_mcp, MCP_HORN_PORTA_PIN, MCP_HORN_ACTIVE_HIGH == 1);
@@ -13,18 +15,18 @@ static void onButtonPress() {
   lv_obj_t* const active_screen = lv_scr_act();
   if (ui_ScreenMain != nullptr && active_screen == ui_ScreenMain) {
     touch_inject_press(358, 60, 60);
-    Serial.println("Button Pressed in Main Screen");
+    LOGI("Button pressed in main screen");
     g_horn.pulse(150);
   }
   else {
-    Serial.println("Button Pressed but ignored - not in Main Screen");
+    LOGI("Button pressed but ignored - not in main screen");
   }
   
 }
 
 //TODO figure our how to add parameters to callback this code doesnt work lol
 // static void onButtonPress(int x, int y, int button_ms, int horn_ms) {
-//   Serial.println("Button Pressed");
+//   LOGI("Button pressed");
 //   touch_inject_press(x, y, button_ms);
 //   g_horn.pulse(horn_ms);
 // }
